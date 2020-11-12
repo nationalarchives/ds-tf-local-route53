730d9573c67909787635db616b074b7fdd70a40a
domingobishop <domingobishop@gmail.com>
2020-11-12 14:50:55 +0000

    add editor config

--- route53.tf
+++ route53.tf
@@ -2,17 +2,17 @@
 # Private zone
 # ------------------------------------------------------------------------------
 resource "aws_route53_zone" "private_zone" {
-  name = "${var.environment}.local"
-  vpc {
-    vpc_id = var.vpc_id
-  }
+    name = "${var.environment}.local"
+    vpc {
+        vpc_id = var.vpc_id
+    }
 
-  tags = {
+    tags = {
         Environment = var.environment
-        Service     = var.service
-        Owner       = var.owner
-        CreatedBy   = var.created_by
-        CostCentre  = var.cost_centre
-        Terraform   = "true"
+        Service = var.service
+        Owner = var.owner
+        CreatedBy = var.created_by
+        CostCentre = var.cost_centre
+        Terraform = "true"
     }
 }
